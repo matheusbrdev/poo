5e0e6be0e1b2deb8cd6fb87268a2f20f6b0a9f3c
Matheus Ribeiro <matheusbrdev@gmail.com>
2023-08-09 08:21:20 -0300

    as chaves das funcoes foram colocadas nas linhas abaixo

--- 01_exemplo_procedural/funcoes.php
+++ 01_exemplo_procedural/funcoes.php
@@ -4,26 +4,32 @@ require_once __DIR__ . '/../utilitarios.php';
 
 //Passagem por referência
 
-function setCor(&$cor, string $corArgument): void {
+function setCor(&$cor, string $corArgument): void
+{
     $cor = $corArgument;
 }
 
-function setPortas(&$portas, int $portasArgument): void {
+function setPortas(&$portas, int $portasArgument): void
+{
     $portas = $portasArgument;
 }
 
-function setTransmissaoManual(&$transmissaoManual, bool $value): void {
+function setTransmissaoManual(&$transmissaoManual, bool $value): void
+{
     $transmissaoManual = $value;
 }
 
-function acelerar(&$velocidade): void {
+function acelerar(&$velocidade): void
+{
     $velocidade += 5;
 }
 
-function frear(&$velocidade): void {
+function frear(&$velocidade): void
+{
     $velocidade = 0;
 }
 
-function ligarFarois(&$faroisLigados): void {
+function ligarFarois(&$faroisLigados): void
+{
     $faroisLigados = true;
 }
