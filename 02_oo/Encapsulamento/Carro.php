b2d1e50a669718dbece4c7dfd64d7c12affba0f1
Matheus Ribeiro <matheusbrdev@gmail.com>
2023-08-11 08:44:14 -0300

    Feat: Aula de encapsulamento

---
+++ 02_oo/Encapsulamento/Carro.php
@@ -0,0 +1,47 @@
+<?php
+
+class Carro 
+{
+    private string $cor;
+    private int $portas;
+    private int $velocidade;
+    public bool $faroisLigados;
+    public bool $transmissaoManual;
+
+    public function __construct(int $portas, string $cor)
+    {
+        $this->portas = $portas;
+        $this->cor = $cor;
+    }
+
+    public function getCor(): string 
+    {
+        return $this->cor;
+    }
+
+    public function setVelocidade(int $velocidade): void
+    {
+        if($velocidade < 0) {
+            echo "Não é possível setar uma velocidade negativa";
+
+            return;
+        }
+
+        $this->velocidade = $velocidade;
+    }
+
+    public function getVelocidade(): int
+    {
+        return $this->velocidade;
+    }
+
+    public function setFarois(bool $faroisLigados): void
+    {
+        $this->faroisLigados = $faroisLigados;
+    }
+
+    public function setTransmissaoManual(bool $transmissaoManual): void
+    {
+        $this->transmissaoManual = $transmissaoManual;
+    }
+}
---
+++ 02_oo/Encapsulamento/teste.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "Carro.php";
+
+$carro = new Carro(4, 'Branco');
+
+$carro->setVelocidade(40);
+
+echo $carro->getVelocidade();
